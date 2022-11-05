2a5766d48c8e9bd16e5a702a749e248ff195cf89
David Kowis <david@kow.is>
2022-11-05 17:27:30 -0500

    I think this will properly tag things

---
+++ .github/actions/get_latest_stable.mjs
@@ -0,0 +1,55 @@
+import OctoKit from "@octokit/action";
+import {graphql} from "@octokit/graphql";
+import fs from 'fs';
+// want to get the apis for Powerdns and tags and filter out things for what we want
+
+const graphqlWithAuth = graphql.defaults({
+    headers: {
+        authorization: `token ${process.env.GITHUB_TOKEN}`
+    }
+});
+
+async function versionsForProduct(productPrefix) {
+    const recursorVersions = await graphqlWithAuth(
+        `
+            query MyQuery {
+            repository(name: "pdns", owner: "PowerDNS") {
+            refs(refPrefix: "refs/tags/", last: 20, query: "${productPrefix}") {
+            nodes {
+            name
+            prefix
+            }
+            totalCount
+            }
+            }
+            }
+            `
+    );
+
+    const versions = recursorVersions.repository.refs.nodes
+    const filtered = versions.filter((item) => {
+        //If the name includes a hyphen, then it's got a alpha or pre-release we don't want
+        return !item.name.replace(productPrefix, '').includes('-');
+    });
+
+    const latestStable = filtered.at(-1).name.replace(productPrefix, '');
+    return latestStable;
+}
+
+const latestStableRecursor = await versionsForProduct("rec-");
+const latestStableDnsdist = await versionsForProduct("dnsdist-");
+const latestStableAuth = await versionsForProduct("auth-");
+
+const data =
+    `recursorVersion=${latestStableRecursor}
+dnsdistVersion=${latestStableDnsdist}
+authVersion=${latestStableAuth}`;
+
+if (process.env.GITHUB_OUTPUT) {
+    fs.writeFile(process.env.GITHUB_OUTPUT, data, (err) => {
+        // In case of a error throw err.
+        if (err) throw err;
+    })
+} else {
+    console.log(data);
+}
